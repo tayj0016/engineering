98bd867e03f774decb4d5b218bdc27061fdad7fb
tayj0016 <tayj0016@gmail.com>
2023-01-26 21:36:39 -0500

    nodes

--- extras/nodes.py
+++ extras/nodes.py
@@ -2,81 +2,59 @@ import pandas
 import pandas as pd
 from operator import itemgetter
 
-# SET CONSTANTS
-c1 = 8.5
-c2 = 6.99
+poles_num = input("How many poles (2 or 4): ")
+poles_num = int(poles_num)
 
-# poles_num = input("How many poles (2 or 4):")
-#
-# while poles_num not in (2, 4):
-#     poles_num = input("Invalid entry. How many poles (2 or 4): ")
-#     poles_num = int(poles_num)
-#
-# if poles_num == 2:
-#     poles = {
-#         'poleA': [],
-#         'poleB': [],
-#     }
-# else:
-#     poles = {
-#         'poleA': [],
-#         'poleB': [],
-#         'poleC': [],
-#         'poleD': [],
-#     }
+while poles_num not in (2, 4):
+    poles_num = input("Invalid entry. How many poles (2 or 4): ")
+    poles_num = int(poles_num)
 
-poles = {
-    'poleA': [10, 20],
-    'poleB': [5, 15],
-    'poleC': [4, 30],
-    'poleD': [12, 50],
-}
+if poles_num == 2:
+    poleA = ['poleA']
+    poleB = ['poleB']
+    poles = [poleA, poleB]
+else:
+    poleA = ['poleA']
+    poleB = ['poleB']
+    poleC = ['poleC']
+    poleD = ['poleD']
+    poles = [poleA, poleB, poleC, poleD]
 
+i = 0
+for x in poles:
+    sta = input("Enter a station for POLE " + poles[i][0] + " ")
+    off = input("Enter a offset for POLE " + poles[i][0] + " ")
+    x.append(sta)
+    x.append(off)
+    i += 1
 
-# for x in poles:
-#     station = input("Input " + x + " station:")
-#     station = int(station)
-#     poles[x].append(station)
-#     offset = input("Input " + x + " offset:")
-#     offset = int(offset)
-#     poles[x].append(offset)
+# TESTING VARIABLES
+# poles_num = 4
+# poleA = ['poleA', 30, 20]
+# poleB = ['poleB', 45, 25]
+# poleC = ['poleC', 25, 15]
+# poleD = ['poleD', 50, 20]
+# poles = [poleA, poleB, poleC, poleD]
 
-# FIND LOWEST STATION
-low1 = None
-low2 = None
+# FIND THE LOWEST STATION
+print(poles)
+poles.sort(key=itemgetter(1))
+print(poles)
 
-# minval = min(poles.values())
-# print(minval)
-#
-# N = 2
-# res = sorted(poles.items())
-# print(res)
-# res = dict(sorted(test_dict.items(), key = itemgetter(1), reverse = True)
-# print(res)
+if poles[0][2] < poles[1][2]:
+    set_pole = poles[0]
+elif poles[1][2] < poles[0][2]:
+    set_pole = poles[1]
 
-i = len(poles)
-for x in range(0, len(poles)):
-    print(poles[x+1][0])
+try:
+    print("the set pole is " + set_pole[0])
+except:
+    print("Cannot find set pole")
 
+# FIND ORIGIN
+origin = ['origin']
 
-    # print(poles[x])
-
-
-# FIND HIGHEST OFFSET
-
-# SET UP NODES WITH [X, Y]
-# nodes = {
-#     'n1': [1, 2],
-#     'n2': [5, 7],
-#     'n3': [6, 8],
-#     'n4': [2, 4],
-#     'n5': [8, 9]
-# }
-
-# CALCULATE AND ADD Z TO NODES
-# for el in nodes:
-#     nodes[el].insert(3, nodes[el][1] * c1)
-#
+# EXPORT TO DATAFRAME
 # df = pd.DataFrame.from_dict(nodes, orient='index').reset_index()
 # df = df.iloc[:, 1:4]
 # df.columns = ['x', 'y', 'z']
